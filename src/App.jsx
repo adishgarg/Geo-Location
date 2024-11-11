d6a519ab020f34bf1bc87c5195d9dcdaf6a47a0c
adishgarg <gargadi456@gmail.com>
2024-11-11 15:35:06 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,211 +1,55 @@
-// import React, { useEffect } from 'react';
-// import L from 'leaflet';
-// import 'leaflet/dist/leaflet.css';
-// import 'leaflet-routing-machine';
-
-// const MapComponent = () => {
-//   useEffect(() => {
-//     // Check if the map is already initialized
-//     if (L.DomUtil.get('map') !== null) {
-//       L.DomUtil.get('map')._leaflet_id = null;
-//       if (navigator.geolocation) {
-//         navigator.geolocation.watchPosition(position => {
-//           const userLat = position.coords.latitude;
-//           const userLng = position.coords.longitude;
-//           L.marker([userLat, userLng]).addTo(map).bindPopup('You are here').openPopup();
-//           map.setView([userLat, userLng], 15);
-    
-//           // Routing
-//           L.Routing.control({
-//             waypoints: [
-//               L.latLng(userLat, userLng),
-//               L.latLng([30.51505986493433, 76.6607994540213])
-//             ],
-//             routeWhileDragging: true
-//           }).addTo(map);
-//         });
-//       }
-//     }
-
-//     // Initialize the map
-//     const map = L.map('map', {
-//       center: [30.516143776540222, 76.6597691858834],
-//       zoom: 18,
-//       maxBounds: [
-//         1,1  // Northeast coordinates
-//       ],
-//       maxBoundsViscosity: 1.0,
-//       // scrollWheelZoom: false 
-//     });
-
-//     // Set up the OSM tile layer
-//     L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-//       // maxZoom: 19,
-//   // attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-//     }).addTo(map);
-
-//     // Add a marker for the university
-//     L.marker([30.516143776540222, 76.6597691858834]).addTo(map)
-//       .bindPopup('Chitkara university')
-//       .openPopup();
-//       // Add a marker for the university
-//     L.marker([30.515918458264974, 76.65811797951999]).addTo(map)
-//       .bindPopup('Sportorium')
-//       .openPopup();
-//     L.marker([30.515826029960863, 76.65693780802657]).addTo(map)
-//       .bindPopup('Explortorium')
-//       .openPopup();
-//     L.marker([30.51725761674037, 76.65917602902984]).addTo(map)
-//       .bindPopup('Alpha Zone')
-//       .openPopup();
-//     L.marker([30.51535691489443, 76.65980128380376]).addTo(map)
-//       .bindPopup('Square 1')
-//       .openPopup();
-//     L.marker([30.51736741565043, 76.66066507961823]).addTo(map)
-//       .bindPopup('Square 2')
-//       .openPopup();
-//     L.marker([30.51505986493433, 76.6607994540213]).addTo(map)
-//       .bindPopup('Omega Zone')
-//       .openPopup();
-//     L.marker([30.51656740863404, 76.65809926671677]).addTo(map)
-//       .bindPopup('Zero Ground')
-//       .openPopup();
-//     L.marker([30.51568241619399, 76.65977982058816]).addTo(map)
-//       .bindPopup('Beta Zone')
-//       .openPopup();
-//     L.marker([30.5140770746809, 76.65990430496825]).addTo(map)
-//       .bindPopup('Rockfeller')
-//       .openPopup();
-    
-      
-//     // Cleanup function to remove the map on component unmount
-//     return () => {
-//       map.remove();
-//     };
-//   }, []);
-
-//   return <div id="map" style={{ height: '100vh' }}></div>;
-// };
-
-// export default MapComponent;
-
 import React, { useEffect } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-routing-machine';
 
-const MapComponent = () => {
+const Map = () => {
   useEffect(() => {
-    let map;
-    let watchID;
-    
-    // Function to initialize map and routing
-    const initializeMap = (userLat, userLng) => {
-      // Set map view to user's location
-      map.setView([userLat, userLng], 15);
+    // Initialize the map
+    const map = L.map('map', {
+      center: [30.516198, 76.659730],
+      zoom: 15,
+    });
 
-      // Add marker for user's location
-      const userMarker = L.marker([userLat, userLng])
-        .addTo(map)
-        .bindPopup('You are here')
-        .openPopup()
-        .dragging.disable();
+    // Add the tile layer
+    L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.{ext}', {
+      minZoom: 0,
+      maxZoom: 20,
+      attribution: '',
+      ext: 'jpg',
+    }).addTo(map);
 
-      // Add routing from user's location to a predefined destination
-      L.Routing.control({
-        waypoints: [
-          L.latLng(userLat, userLng),
-          L.latLng([30.51505986493433, 76.6607994540213])
-        ],
-        // routeWhileDragging: true
-      }).addTo(map);
-    };
+    // Add a marker to the map
+    L.marker([30.514964084623355, 76.66102511416453], { draggable: false })
+      .addTo(map)
+      .bindPopup("Omega Ground")
+      .openPopup();
+    L.marker([30.51587231653969, 76.65719118779487], { draggable: false })
+      .addTo(map)
+      .bindPopup("Explortorium")
+      .openPopup();
+    L.marker([30.51570445886611, 76.65657071175687], { draggable: false })
+      .addTo(map)
+      .bindPopup("Tesla Block")
+      .openPopup();
+    L.marker([30.51569279880044, 76.65807396029054], { draggable: false })
+      .addTo(map)
+      .bindPopup("Sportorium")
+      .openPopup();
+    L.marker([30.517043690210198, 76.65939857007433], { draggable: false })
+      .addTo(map)
+      .bindPopup("Alpha Zone")
+      .openPopup();
 
-    if (navigator.geolocation) {
-      // Request location updates with high accuracy
-      watchID = navigator.geolocation.watchPosition(
-        position => {
-          const userLat = position.coords.latitude;
-          const userLng = position.coords.longitude;
-
-          // Initialize map only if it hasn't been created yet
-          if (!map) {
-            map = L.map('map', {
-              center: [userLat, userLng],
-              // zoom: 18,
-              // maxBounds: [ 1,1],
-              // maxBoundsViscosity: 1.0,
-            });
-
-            // Add tile layer to the map
-            L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-              // maxZoom: 19,
-            }).addTo(map);
-
-            // Add markers for different locations
-           //     L.marker([30.516143776540222, 76.6597691858834]).addTo(map)
-//       .bindPopup('Chitkara university')
-//       .openPopup();
-//       // Add a marker for the university
-          L.marker([30.515918458264974, 76.65811797951999]).addTo(map)
-            .bindPopup('Sportorium')
-            .openPopup();
-          L.marker([30.515826029960863, 76.65693780802657]).addTo(map)
-            .bindPopup('Explortorium')
-            .openPopup();
-          L.marker([30.51725761674037, 76.65917602902984]).addTo(map)
-            .bindPopup('Alpha Zone')
-            .openPopup();
-          L.marker([30.51535691489443, 76.65980128380376]).addTo(map)
-            .bindPopup('Square 1')
-            .openPopup();
-          L.marker([30.51736741565043, 76.66066507961823]).addTo(map)
-            .bindPopup('Square 2')
-            .openPopup();
-          L.marker([30.51505986493433, 76.6607994540213]).addTo(map)
-            .bindPopup('Omega Zone')
-            .openPopup();
-          L.marker([30.51656740863404, 76.65809926671677]).addTo(map)
-            .bindPopup('Zero Ground')
-            .openPopup();
-          L.marker([30.51568241619399, 76.65977982058816]).addTo(map)
-            .bindPopup('Beta Zone')
-            .openPopup();
-          L.marker([30.5140770746809, 76.65990430496825]).addTo(map)
-            .bindPopup('Rockfeller')
-            .openPopup();
-            // Add other markers here...
-
-            // Initialize the map and routing with the user's position
-            initializeMap(userLat, userLng);
-          } else {
-            // Update the marker and map view for the current position
-            map.setView([userLat, userLng], 15);
-          }
-        },
-        error => {
-          console.error('Error getting location: ', error);
-        },
-        {
-          enableHighAccuracy: true, // High accuracy mode
-          timeout: 5000, // Timeout after 5 seconds
-          maximumAge: 0, // Don't use cached location
-        }
-      );
-    }
-
-    // Cleanup function to remove map and stop watching location
+    // Cleanup function to remove the map instance when the component unmounts
     return () => {
-      if (map) {
-        map.remove();
-      }
-      if (watchID) {
-        navigator.geolocation.clearWatch(watchID);
-      }
+      map.remove();
     };
   }, []);
 
-  return <div id="map" style={{ height: '100vh' }}></div>;
+  return (
+    <div id="map" style={{ height: '100vh', width: '100%' }}></div>
+  );
 };
 
-export default MapComponent;
+export default Map;
